e79549dbf6e6151971efdeb8aa515a4133aa7f0a
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-10 15:43:03 +0300

    build the about us section

--- src/Components/SectionHeader/SectionHeader.jsx
+++ src/Components/SectionHeader/SectionHeader.jsx
@@ -1,14 +1,13 @@
-import "./SectionHeader.css"
-import React from 'react'
+import "./SectionHeader.css";
+import React from "react";
 
-function SectionHeader( {h2, text}) {
+function SectionHeader({ h2, text }) {
   return (
     <div className="section-header-title">
       <h2 className="section-title">{h2}</h2>
       <h3 className="section-text">{text}</h3>
-      
     </div>
-  )
+  );
 }
 
-export default SectionHeader
+export default SectionHeader;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import Video from "../../../public/cheetah-video.mp4";
-import "./Home.css"
+import "./Home.css";
 import { Fragment } from "react";
 import SectionHeader from "../../Components/SectionHeader/SectionHeader";
 
@@ -7,25 +7,43 @@ function Home() {
   return (
     <Fragment>
       <HeroSection />
-      <SectionHeader h2='About Us' text="A Community of Luxury Travelers"/>
+      <SectionHeader h2="About Us" text="A Community of Luxury Travelers" />
+      <AboutUsDescription />
     </Fragment>
   );
 }
 
-function HeroSection () {
+function HeroSection() {
   return (
     <section className="hero-section">
       <video autoPlay muted loop playsInline className="background-video">
-  <source src={Video} type="video/mp4" />
-  Your browser does not support the video tag.
-</video>
+        <source src={Video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
       <div className="overlay"></div>
       <div className="hero-text">
-        <h2>Travel <span> * </span> Discover <span> * </span> Belong</h2>
+        <h2>
+          Travel <span> * </span> Discover <span> * </span> Belong
+        </h2>
       </div>
     </section>
-  )
+  );
+}
+
+function AboutUsDescription() {
+  return (
+    <div className="about-us-text-container">
+      <p className="about-us-text">
+        Zaph Tours is a trusted Community of modern luxury travelers. Find
+        Inspiration, book unique journeys, connect with like minded members.
+      </p>
+      <div className="about-us-ctn">
+        <img src="../../src/assets/about-us-img.jpg" alt="safari-photo" />
+        <a href="#">join us</a>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
