fa7129625df048752814a896edd7408b2267f9f5
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-10 14:27:57 +0300

    build the hero section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,28 @@
+import Video from "../../../public/cheetah-video.mp4";
+import "./Home.css"
+import { Fragment } from "react";
 function Home() {
   return (
-    <div>
-      <h1>This is the Home Page</h1>
-    </div>
+    <Fragment>
+      <HeroSection />
+    </Fragment>
   );
+}
+
+function HeroSection () {
+  return (
+    <section className="hero-section">
+      <video autoPlay muted loop playsInline className="background-video">
+  <source src={Video} type="video/mp4" />
+  Your browser does not support the video tag.
+</video>
+
+      <div className="overlay"></div>
+      <div className="hero-text">
+        <h2>Travel <span> * </span> Discover <span> * </span> Belong</h2>
+      </div>
+    </section>
+  )
 }
 
 export default Home;
